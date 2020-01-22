1b51b1147755e7ac3ea126deeabfe136a8856a5f
Muhammad Rachim <mac@macs-MacBook-Air.local>
2020-01-23 00:49:03 +0700

    add main page for admin page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Components from './components/Components'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/_main.scss'
+import Dashboard from './pages/admin/Dashboard';
 
 
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Components.navbar />
+      <Dashboard />
     </>
   );
 }
