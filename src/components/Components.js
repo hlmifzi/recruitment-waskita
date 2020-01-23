080ff17bc3d1c186b7ad30c4bd08d411d5ab0957
helmi <helmi@happy5.co>
2020-01-23 12:36:18 +0700

    standart of chart

--- src/components/Components.js
+++ src/components/Components.js
@@ -1,7 +1,13 @@
 import MainNavbar from './navbar/MainNavbar'
+import BarChart from './charts/BarChart'
+import PieChart from './charts/PieChart'
 
 const Components = {
-    navbar: MainNavbar
+    navbar: MainNavbar,
+    chart: {
+        barChart: BarChart,
+        pieChart: PieChart
+    }
 }
 
 export default Components
