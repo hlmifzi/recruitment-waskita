20fb04007fa9f6a29a7951dd73ed1526808a3867
Muhammad Rachim <mac@macs-MacBook-Air.local>
2020-01-27 23:51:14 +0700

    add header layout for modal

--- src/components/modal/ModalResult.js
+++ src/components/modal/ModalResult.js
@@ -1,5 +1,6 @@
 
 import React, { useEffect, useState, useRef } from 'react'
+import iconDownload from '../../assets/download.svg'
 
 const ModalResult = ({closeModal}) => {
   const wrapperRef = useRef(null);
@@ -21,8 +22,13 @@ const ModalResult = ({closeModal}) => {
   return (
       <>
         <div className="modal-result">
-          <div className="modal-card" ref={wrapperRef}>
-
+          <div className="modal-result-card" ref={wrapperRef}>
+            <div className="modal-result-header">
+              <p>Result</p>
+              <span className="icon-download img-rounded">
+                <img src={iconDownload} />
+              </span>
+            </div>
           </div>
         </div>
       </>
