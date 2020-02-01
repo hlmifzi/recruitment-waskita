93ebb74e29781acb1020bfbda5a7f7ea08e2a4a6
helmi <helmi@happy5.co>
2020-02-02 01:14:10 +0700

    revisi logout

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import logo from '../../logo-waskita.png'
+import { useApolloClient } from "@apollo/react-hooks";
 
 const MainNavbar = () => {
+    const client = useApolloClient();
+
+    const _handleSignout = () => {
+        localStorage.clear()
+        client.writeData({ data: { isLoggedIn: false } });
+    }
+
     return (
         <>
             <div className="header-admin">
                 <img src={logo} />
-                <div class="pull-right text-white"> log out </div>
+                <button class="pull-right text-white" onClick={_handleSignout}> log out </button>
             </div>
         </>
     )
