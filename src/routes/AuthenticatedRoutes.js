4eb8d94491b26bc2d846d03b08c2620d3219e0e9
helmi <helmi@happy5.co>
2020-02-01 13:31:45 +0700

    default authenticated app

--- src/routes/AuthenticatedRoutes.js
+++ src/routes/AuthenticatedRoutes.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import SignIn from '../pages/auth/SignIn';
 import Admin from '../pages/admin/Admin'
 import { Router } from "@reach/router"
 
 const AuthenticatedRoutes = () => {
     return (
         <Router>
-            <SignIn path="/" />
             <Admin path="/admin" />
+            <Admin default />
         </Router>
     );
 }
