55771d45b261c9d705304bef798bf972ab5b98a6
Muhammad Rachim <mac@macs-MacBook-Air.local>
2020-02-12 02:28:05 +0700

    component download facebook info (done)

--- src/components/recruitment/FacebookInfo.js
+++ src/components/recruitment/FacebookInfo.js
@@ -27,6 +27,68 @@ const FacebookInfo = () => {
           </div>
           <label className="tag-header facebook-color">Download Facebook Information</label>
         </div>
+        <div className="d-flex mt-48">
+          <div className="container-step">
+            <div className="label-step"><span className="mr-6">1</span>Click</div>
+            <p className="text-center"><strong>Facebook Account</strong></p>
+            <div className="wrapper-image-circle">
+              <img src={facebookIcon} />
+            </div>
+          </div>
+          <div className="container-step">
+            <div className="label-step"><span className="mr-6">2</span>Click</div>
+            <p className="text-center"><strong>Settings</strong></p>
+            <div className="wrapper-image-circle">
+              <img src={settingIcon} />
+            </div>
+          </div>
+          <div className="container-step">
+            <div className="label-step"><span className="mr-6">3</span>Click</div>
+            <p className="text-center"><strong>Your Facebook Information</strong></p>
+            <div className="wrapper-image-circle">
+              <img src={desktopIcon} />
+            </div>
+          </div>
+          <div className="container-step">
+            <div className="label-step"><span className="mr-6">4</span>Click</div>
+            <p className="text-center"><strong>Download Your Information</strong></p>
+            <div className="wrapper-image-circle">
+              <img src={folderIcon} />
+            </div>
+            <span className="dummy-border-dashed dummy-border-left">
+              <i className="fa fa-chevron-up" />
+            </span>
+          </div>
+          <div className="container-instruction-box">
+            <div className="instruction-box">
+              <p className="mb-1"><strong>Select:</strong></p>
+              <p className="mb-1">1. Post</p>
+              <p className="mb-1">2. Comment</p>
+              <p className="mb-1">3. Like & Reaction</p>
+              <p className="mb-1">4. Friends</p>
+              <p className="mb-1">5. Groups</p>
+              <p className="mb-1">6. Events</p>
+              <p className="mb-1">7. Profile Information</p>
+            </div>
+          </div>
+          <div className="container-step">
+            <div className="label-step"><span className="mr-6">5</span>Click</div>
+            <p className="text-center"><strong>Create File</strong></p>
+            <div className="wrapper-image-circle">
+              <img src={fileIcon} />
+            </div>
+            <span className="dummy-border-dashed dummy-border-right">
+              <i className="fa fa-chevron-up" />
+            </span>
+          </div>
+          <div className="container-step">
+            <div className="label-step"><span className="mr-6">6</span>Click</div>
+            <p className="text-center"><strong>Download</strong></p>
+            <div className="wrapper-image-circle">
+              <img src={folderDownloadIcon} />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
