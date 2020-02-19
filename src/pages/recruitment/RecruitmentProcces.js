00935dddfd3a5d8ae01cc9dbf4e2a002c19b0185
Muhammad Rachim <mac@macs-MacBook-Air.local>
2020-02-19 21:12:54 +0700

    add modal remind

--- src/pages/recruitment/RecruitmentProcces.js
+++ src/pages/recruitment/RecruitmentProcces.js
@@ -1,43 +1,77 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import Components from '../../components/Components'
 import FacebookInfo from './view/FacebookInfo'
 import InstagramInfo from './view/InstagramInfo'
 import TwitterInfo from './view/TwitterInfo'
+import iconBell from '../../assets/recruitment/bell-with-attention.svg'
 
 
 const RecruitmentProcces = props => {
 
-    const [hasDownload, setHasDownload] = useState(false)
-    const [currentInstruction, setInstruction] = useState(1)
-    const [currentStep, setStep] = useState(1)
+  const [hasDownload, setHasDownload] = useState(false)
+  const [showModal, setShowModal] = useState(false)
+  const [currentInstruction, setInstruction] = useState(1)
+  const [currentStep, setStep] = useState(1)
+  const wrapperRef = useRef()
 
-    const nextStep = () => {
-        if(hasDownload){
-            setStep(currentStep + 1)
-        }
-        else if(currentInstruction >= 3){
-            setHasDownload(true)
-        }
-        else {
-            setInstruction(currentInstruction + 1)
-        }
+  const nextStep = () => {
+    if (hasDownload) {
+      setStep(currentStep + 1)
     }
+    else if (currentInstruction == 3) {
+      setTimeout(() => {
+        setShowModal(true)
+        document.body.classList.add("scroll-locked")
+      },100)
+    }
+    else {
+      setInstruction(currentInstruction + 1)
+    }
+  }
 
-    return (
-        <div className="body-dashboard">
-            <Components.progressNavbar />
-            <Components.recruitmentCard nextStep={() => nextStep()}>
-                {
-                    <>
-                        { (!hasDownload && currentInstruction == 1) && <FacebookInfo /> }
-                        { (!hasDownload && currentInstruction == 2) && <InstagramInfo /> }
-                        { (!hasDownload && currentInstruction == 3) && <TwitterInfo /> }
-                    </>
-                }
-            </Components.recruitmentCard>
+  const clickOutsideModal = event => {
+    if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+      document.body.classList.remove("scroll-locked")
+      setShowModal(false)
+    }
+  };
 
+  const confirmDownload = () => {
+    setHasDownload(true)
+    setShowModal(false)
+    document.body.classList.remove("scroll-locked")
+  }
+
+  useEffect(() => {
+    document.addEventListener("click", clickOutsideModal, false);
+    return () => {
+      document.removeEventListener("click", clickOutsideModal, false);
+    };
+  }, [])
+
+  return (
+    <div className="body-dashboard">
+      {showModal &&
+        <div className="modal-remind">
+          <div className="remind-card" ref={wrapperRef}>
+            <img src={iconBell} />
+            <h4 className="text-center">Are you sure,<br />You have downloaded all social media information ?</h4>
+            <button onClick={() => confirmDownload()}>Next</button>
+          </div>
         </div>
-    )
+      }
+      <Components.progressNavbar />
+      <Components.recruitmentCard nextStep={() => nextStep()}>
+        {
+          <>
+            {(!hasDownload && currentInstruction == 1) && <FacebookInfo />}
+            {(!hasDownload && currentInstruction == 2) && <InstagramInfo />}
+            {(!hasDownload && currentInstruction == 3) && <TwitterInfo />}
+          </>
+        }
+      </Components.recruitmentCard>
+    </div>
+  )
 }
 
 export default RecruitmentProcces
