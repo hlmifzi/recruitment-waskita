c63caf5991bcd60630cb75d6c1f35e7b580edb6e
helmi <helmi@happy5.co>
2020-02-27 10:50:04 +0700

    adain chart perempuan lagi

--- src/pages/admin/ParticipantStatistic.js
+++ src/pages/admin/ParticipantStatistic.js
@@ -25,7 +25,7 @@ const ParticipantStatistic = () => {
   return (
     <Row>
       <Components.charts.lineChart title="Jumlah Peserta" isLoading={isLoadingJumlahPeserta} colMd={12} data={dataJumlahPeserta} />
-      {/* <Components.charts.donatChart title="Jenis Kelamin" isLoading={isLoadingJenisKelamin} colMd={4} data={dataJenisKelamin} /> */}
+      <Components.charts.donatChart title="Jenis Kelamin" isLoading={isLoadingJenisKelamin} colMd={4} data={dataJenisKelamin} />
       <Components.charts.barChart title="Universitas" isLoading={isLoadingUniversitas} colMd={6} data={dataUniversitas} />
       <Components.charts.barChart title="Usia" isLoading={isLoadingUsia} colMd={6} data={dataUsia} />
     </Row>
